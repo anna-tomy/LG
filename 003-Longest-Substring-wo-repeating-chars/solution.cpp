4e507409dd861b0ce771d9407e44fded02c700b9
anna-tomy <24b132.anna@gectcr.ac.in>
2026-01-30 10:10:09 +0530

    solution.cpp

---
+++ 003-Longest-Substring-wo-repeating-chars/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> lastIndex(256,-1);
+        int maxLength=0, left=0;
+        for(int right=0;right<s.length();right++){
+            if(lastIndex[s[right]]>=left) left=lastIndex[s[right]]+1;
+            lastIndex[s[right]]=right;
+            maxLength=max(maxLength,right-left+1);
+        }return maxLength;
+    }
+};
